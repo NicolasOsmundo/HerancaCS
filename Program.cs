30775883531340ba1c7563f24ec45729acbf03c4
NicolasOsmundo <nicolasosmundo@hotmail.com>
2021-03-26 17:47:05 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HerançaCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
